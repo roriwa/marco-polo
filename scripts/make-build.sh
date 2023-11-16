4df8ff89d83117571b0cb4a0e06626d039ac6b22
roriwa <robinrwalter2002@gmail.com>
2023-11-16 14:45:58 +0100

    added build script

---
+++ scripts/make-build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+ROOT="$(realpath "$(dirname "$(realpath "$0")")/../")"
+cd "$ROOT"
+
+SRC=src/obsidian-search/
+DST=dist/obsidian-search/
+
+cd "$SRC"
+npm run build
+
+cd "$ROOT"
+
+if [ -d "$DST" ]; then
+  rm -rf "$DST"
+fi
+mkdir -p "$DST"
+
+cp "$SRC"/manifest.json "$DST"
+cp "$SRC"/main.js "$DST"
+cp "$SRC"/styles.css "$DST"
+
+cd dist/
+if [ -f obsidian-vault.tgz ]; then
+  rm obsidian-vault.tgz
+fi
+tar -czf obsidian-vault.tgz obsidian-search/
